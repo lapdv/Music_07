0ce2bd8999134b11502d6a0fab3a298c6bd28b78
lapdv <lapdv@deha-soft.com>
2018-05-04 11:23:11 +0700

    init main screen

---
+++ app/src/main/java/com/mobile/lapdv/mymusic/screen/main/view/MainView.java
@@ -0,0 +1,10 @@
+package com.mobile.lapdv.mymusic.screen.main.view;
+
+import com.mobile.lapdv.mymusic.base.mvp.BaseView;
+
+/**
+ * Created by lap on 04/05/2018.
+ */
+
+public interface MainView extends BaseView {
+}
